0a2acb4a7636e1fba319f8fa65dcfc1f0254e6a3
Anilkumar <97604348+Anilkumargalipelli@users.noreply.github.com>
2022-12-02 20:48:35 +0530

    Delete spring-data-jpa-component-mapping directory

--- spring-data-jpa-component-mapping/src/main/resources/sql-references/05-Component-Mapping-employee.sql
+++
@@ -1,9 +0,0 @@
-create table employee(
-id int PRIMARY KEY AUTO_INCREMENT,
-name varchar(20),
-streetaddress varchar(30),
-city varchar(20),
-state varchar(20),
-zipcode varchar(20),
-country varchar(20)
-)
